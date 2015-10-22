fc7d894839a6bff9b2e6dbe4e8e776bc3ce1ed04
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-10-22 22:07:07 +0000

    Refactored the site into a front page login app, and a project app

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,64 +1,16 @@
 'use strict';
 
-var ords = angular.module('ords', ['ngRoute']);
-
-    // configure our routes
-    ords.config(function($routeProvider) {
-        $routeProvider
-
-            // Home page with projects list
-            .when('/', {
-                templateUrl : 'views/home/home.html',
-                controller  : 'mainController'
-            })
-			
-			// Login form
-            .when('/login', {
-                templateUrl : 'views/home/login.html',
-                controller  : 'mainController'
-            })
-
-            // Project Details
-            .when('/project/:param', {
-                templateUrl : 'views/project/project.html',
-                controller  : 'projectController'
-            })
-			
-			// New Project Form
-			.when('/newproject', {
-                templateUrl : 'views/project/newproject.html',
-                controller  : 'newProjectController'				
-			})
-			
-			// User account preferences
-			
-			// Invite User
-			
-			// Add existing User to Project
-			
-			// Add a database
-			
-			.otherwise({
-				redirectTo: '/'
-			})
-			
-			;
-    });
+var ords = angular.module('ords',[]);
 	
-    ords.controller('mainController', function ($scope, $http, $location) {
+    ords.controller('mainController', function ($scope, $http, $window) {
 		//
-		// If we're logged in, then show project list
-		// Otherwise show the login redirects
+		// If we're logged in, then redirect to the project app
 		//		
     	  $http.get('/api/1.0/user').then(
 			 function successCallback(response) { 
-				 $location.path("/"); 
+				 $window.location.href = "/project";
 				 $scope.user = response.data;
 				 $scope.loggedIn="yes"
-			 }, 
-			 function errorCallback(response) { 
-				 $location.path("/login"); 
-				 $scope.user = null;
 			 }
 		  );  
 		  
