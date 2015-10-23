3a36cc15793d3541da6a53c711f09970395bf21e
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-10-23 13:08:05 +0000

    Simplified front page app

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,25 +1,29 @@
 'use strict';
 
-var ords = angular.module('ords',[]);
+var ords = angular.module('ords',['ngResource'])
+
+//
+// Main REST Resources
+//
+.factory('User', function( $resource ) {
+	return $resource('/api/1.0/user/');
+})
+
 	
-    ords.controller('mainController', function ($scope, $http, $window) {
-		//
-		// If we're logged in, then redirect to the project app
-		//		
-    	  $http.get('/api/1.0/user').then(
-			 function successCallback(response) { 
-				 $window.location.href = "/project";
-				 $scope.user = response.data;
-				 $scope.loggedIn="yes"
-			 }
-		  );  
-		  
-		  //
-		  // Displays any user messages, like success or warnings
-		  //
-		  $scope.$on('messageEvent', function(event, message) {
-			  $scope.showMessage = true;
-		      $scope.message = message;
-		  });  
-	});
+.controller('mainController', function ($scope, $window, User) {
+	//
+	// If we're logged in, redirect to project app
+	//		
+	$scope.user = User.get(
+		 function successCallback() { 
+			 $window.location.href = "/project"; 
+			 $scope.loggedIn="yes"
+		 }, 
+		 function errorCallback() { 
+			 $scope.loggedIn="no"
+		 }				
+	);
+})
+
+;
 	
