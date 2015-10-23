9962e65d5ce6f75b7dd4be16e223bd16f50f0390
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-10-23 13:09:34 +0000

    Refactored the Project app to make it closer to regular Angular apps. Load initial Project data once in Module.run() method, then refresh on creating a new Project (added a manual Refresh button for testing)

--- src/main/webapp/project/app.js
+++ src/main/webapp/project/app.js
@@ -1,63 +1,113 @@
 'use strict';
 
-var ords = angular.module('ords', ['ngRoute','ngResource']);
+var ords = angular.module('ords', ['ngRoute','ngResource', 'angular-growl'])
 
-    // configure our routes
-    ords.config(function($routeProvider) {
-        $routeProvider
+	//
+	// Project REST Resources
+	//
+	.factory('Project', function( $resource ) {
+		return $resource('/project-api/project/:id');
+	})
 
-            // Home page with projects list
-            .when('/', {
-                templateUrl : 'views/home.html',
-                controller  : 'mainController'
-            })
+	.factory('User', function( $resource ) {
+		return $resource('/api/1.0/user/');
+	})
 
-            // Project Details
-            .when('/project/:id', {
-                templateUrl : 'views/project.html',
-                controller  : 'projectController'
-            })
+	.config(['growlProvider', function(growlProvider) {
+	  growlProvider.globalTimeToLive(5000);
+	  growlProvider.globalDisableCountDown(true);
+	}])
+
+	//
+	// configure  routes
+	//
+	.config(function($routeProvider) {
+	        $routeProvider
+
+	            // Home page with projects list
+	            .when('/', {
+	                templateUrl : 'views/home.html',
+	                controller  : 'mainController'
+	            })
+
+	            // Project Details
+	            .when('/project/:id', {
+	                templateUrl : 'views/project.html',
+	                controller  : 'projectController'
+	            })
 			
-			// New Project Form
-			.when('/newproject', {
-                templateUrl : 'views/newproject.html',
-                controller  : 'newProjectController'				
-			})
+				// New Project Form
+				.when('/newproject', {
+	                templateUrl : 'views/newproject.html',
+	                controller  : 'newProjectController'				
+				})
 			
-			// Invite User
+				// Invite User
 			
-			// Add existing User to Project
+				// Add existing User to Project
 			
-			// Add a database
+				// Add a database
 			
-			.otherwise({
-				redirectTo: '/'
-			})
+				.otherwise({
+					redirectTo: '/'
+				})
 			
-			;
-    });
+				;
+	    })
 	
-    ords.controller('mainController', function ($scope, $http, $window) {
+	//
+	// Project Controllers
+	//
+	.controller('mainController', function ($scope, $routeParams, Project) {
+		$scope.refresh = function(){
+			$scope.projects = Project.query(); 	
+		}
+	})
+	
+	.controller('projectController', function ($scope, $routeParams, Project) {
+		$scope.project = Project.get({ id: $routeParams.id });
+	})
+
+	.controller('newProjectController', function ($rootScope, $scope, $http, $location, Project, growl) {
+	
+		//
+		// Submit fields in new project form to create a new project
+		//
+	    $scope.submitNewProject=function(){
+			Project.save($scope.fields,
+				function(){
+					$rootScope.projects = Project.query();
+					growl.success("Project successfully created");
+					$location.path("/");
+				},
+				function(){
+					growl.error("There was a problem creating the project");
+					$location.path("/");
+				}
+			);    
+	     }
+	
+	})
+
+
+	//
+	// Init
+	//
+	.run(function($rootScope, $window, User, Project) {
 		//
 		// If we're not logged in, redirect to login
 		//		
-    	  $http.get('/api/1.0/user').then(
-			 function successCallback(response) { 
-				 $scope.user = response.data;
-				 $scope.loggedIn="yes"
+		$rootScope.user = User.get(
+			 function successCallback() { 
+				 $rootScope.loggedIn="yes"
 			 }, 
-			 function errorCallback(response) { 
+			 function errorCallback() { 
 				 $window.location.href = "/"; 
-				 $scope.user = null;
-			 }
-		  );  
-		  
-		  //
-		  // Displays any user messages, like success or warnings
-		  //
-		  $scope.$on('messageEvent', function(event, message) {
-			  $scope.showMessage = true;
-		      $scope.message = message;
-		  });  
+			 }				
+		);
+
+		//
+		// Load initial projects
+		//
+		$rootScope.projects = Project.query(); 
 	});
-	
--- src/main/webapp/project/components/projectlist/projectlist.js
+++ src/main/webapp/project/components/projectlist/projectlist.js
@@ -2,7 +2,7 @@ angular.module( "ords" ).directive(
 	'projects', 
 	function() {
   	  	return {
-    		controller: 'projectListController',
+    		controller: function(){},
     		templateUrl: 'components/projectlist/projectlist.html'
   	  	}
 	}
