0ee5213080bd43975200517620d072e1841665f2
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-10-29 12:12:15 +0000

    Added AuthCheck and removeMember function

--- src/main/webapp/project/controllers/projectController.js
+++ src/main/webapp/project/controllers/projectController.js
@@ -1,6 +1,31 @@
 'use strict';
 
-ords.controller('projectController', function ($scope, $routeParams, Project) {
-		$scope.project = Project.get({ id: $routeParams.id });
+ords.controller('projectController', function ($scope, $routeParams, AuthService, Project, User, Member, growl) {
+	
+	//
+	// This page doesm't make sense to view
+	// without being logged in, so redirect
+	// back to the home view
+	//
+	AuthService.check();
+	
+	$scope.editMember = function(id){
+		growl.warning("Edit Member clicked");
 	}
-);
+	
+	$scope.removeMember = function(id){
+		Member.delete({ id: $routeParams.id, roleid: id},
+			function(){
+				growl.success("Project member removed");
+			},
+			function(){
+				growl.error("There was a problem removing this project member");				
+			}
+		);
+
+	}
+	
+	$scope.project = Project.get({ id: $routeParams.id });
+	
+	$scope.members = Member.query({ id: $routeParams.id });
+});
