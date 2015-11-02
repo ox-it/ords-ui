9a55e93161d4af1e7c9091951da582523d889de4
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-02 16:44:26 +0000

    Implemented deleting projects

--- src/main/webapp/project/controllers/projectsController.js
+++ src/main/webapp/project/controllers/projectsController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-ords.controller('projectsController', function ($rootScope, $scope, AuthService, Project, $interval, $location) {
+ords.controller('projectsController', function ($rootScope, $scope, AuthService, Project, $interval, $location, growl) {
 	
 	//
 	// This page doesm't make sense to view
@@ -8,6 +8,19 @@ ords.controller('projectsController', function ($rootScope, $scope, AuthService,
 	// back to the home view
 	//
 	AuthService.check();
+	
+	$scope.delete = function(id){
+		Project.delete(
+			{id: id},
+			function(){
+				growl.success("Project deleted");
+				$scope.refresh();
+			},
+			function(){
+				growl.error("There was a problem deleting the project");				
+			});
+
+	}
 	
 	$scope.refresh = function(){
 		Project.query({}, function(data){
