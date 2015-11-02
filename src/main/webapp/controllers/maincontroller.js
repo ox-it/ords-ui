9ec283d23a5c7f85e47e0e6ea8f78dce74d756c4
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-02 16:45:49 +0000

    Use AuthService rather than custom controller code

--- src/main/webapp/controllers/maincontroller.js
+++ src/main/webapp/controllers/maincontroller.js
@@ -1,23 +1,8 @@
-ords.controller('mainController', function($rootScope,$location, User, Project) {
-	
-		//
-		// If we're not logged in, redirect to login
-		//		
-		$rootScope.user = User.get(
-			 function successCallback() { 
-				$rootScope.loggedIn="yes"
-	 			//
-	 			// Load initial projects
-	 			//
-	 			Project.query({}, function(data){
-	 				$rootScope.projects = data;
-	 			});
-				$location.path("/projects"); 
-			 }, 
-			 function errorCallback() { 
-				 $rootScope.loggedIn="no"
-				 $location.path("/"); 
-			 }				
-		);
+ords.controller('mainController', function(AuthService) {
+		
+	//
+	// Conduct auth check
+	//
+	AuthService.check();
 	
 });
