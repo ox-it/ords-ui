aa3f2bf8d483f949bbafcc55ddeec8ee796e4ca6
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2015-11-03 15:42:22 +0000

    Added email verification controller and view

--- src/main/webapp/controllers/verifycontroller.js
+++ src/main/webapp/controllers/verifycontroller.js
@@ -1,9 +1,19 @@
-ords.controller('verifyController', function($rootScope, $scope, $location, User, growl) {
+ords.controller('verifyController', function($rootScope, $scope, $routeParams, $location, $http, growl, gettextCatalog) {
 	
 	//
-	// Submit fields in new project form to create a new project
+	// Verify email
 	//
-	$scope.verify=function(){
-	}
+	var responsePromise = $http.get("/api/1.0/user/verifyemail/" + $routeParams.code);
+		
+	responsePromise.success(function(data, status, headers, config){
+		growl.success( gettextCatalog.getString("Verify200") );
+		$location.path("/projects")
+	});
+	
+	responsePromise.error(function(data, status, headers, config){				
+		if (status === 400){ growl.error(  gettextCatalog.getString("Verify400") ) };
+		if (status === 500){ growl.error(  gettextCatalog.getString("Gen500") ) };	
+		$location.path("/")
+	});
 	
 });
