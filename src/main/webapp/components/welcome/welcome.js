a5d43a5a29a0504b011091426a13e7f189ed1398
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2016-01-29 12:06:53 +0000

    Display a message if the user needs to verify their email

--- src/main/webapp/components/welcome/welcome.js
+++ src/main/webapp/components/welcome/welcome.js
@@ -2,7 +2,21 @@ angular.module( "ords" ).directive(
 	'welcome', 
 	function() {
   	  	return {
-    		controller: function() {},
+    		controller: function($rootScope, $scope, gettextCatalog) {
+				
+				//
+				// Check if the user needs email verification
+				//
+    			$scope.showPendingMessage = function(){
+    				if ($rootScope.user){
+    					if ($rootScope.user.status === "PENDING_EMAIL_VERIFICATION"){
+							$scope.pendingMessage = gettextCatalog.getString("PENDING_EMAIL_VERIFICATION");
+    						return true;
+    					}
+    				}
+					return false;
+    			}
+    		},
     		templateUrl: 'components/welcome/welcome.html'
   	  	}
 	}
