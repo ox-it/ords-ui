67da89a19069fb9b1299fee4705c2bdc6ac71f02
ouit0005 <ouit0005@87df6a52-bba1-4346-a3e1-e27aa7044e28>
2016-01-31 17:06:06 +0000

    Added logout method

--- src/main/webapp/components/profile/profile.js
+++ src/main/webapp/components/profile/profile.js
@@ -2,7 +2,31 @@ angular.module( "ords" ).directive(
 	'profile', 
 	function() {
   	  	return {
-    		controller:  function(){},
+    		controller:  function($rootScope, $scope, $http, $location, growl, gettextCatalog){
+    			
+				//
+				// Perform a logout
+				//
+				$scope.logout = function(){
+					$http(
+						{
+					    method: 'DELETE',
+					    url: "/api/1.0/user/logout",
+					}).then(
+						function successCallback(response){
+							growl.success( gettextCatalog.getString("Logout200") )
+							$rootScope.projects = null;
+							$rootScope.loggedIn="no";
+							$location.path("/");
+						},
+						function errorCallback(response){
+							if (response.status === 400) { growl.error( gettextCatalog.getString("Logout400") ) };
+							if (response.status === 500) { growl.error( gettextCatalog.getString("Gen500") ) };			
+						}
+					);
+				}
+				
+    		},
     		templateUrl: 'components/profile/profile.html'
   	  	}
 	}
